1002ba5b82191eeb8eea5a10e13e027afe039876
garryw28 <garryw2807@gmail.com>
2020-11-27 15:58:10 +0700

    server-streaming

--- Program.cs
+++ Program.cs
@@ -1,12 +1,27 @@
 ﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Grpc.Net.Client;
+using Grpc.Core;
 
 namespace GrpcClientStreaming
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var channel = GrpcChannel.ForAddress("https://localhost:5001");
+            var client =  new Greeter.GreeterClient(channel);
+            using (var call = client.CallStream(new StreamRequest{Request = "test"}))
+            {
+                while(await call.ResponseStream.MoveNext())
+                {
+                    StreamContent content = call.ResponseStream.Current;
+                    Console.WriteLine($"Id : {content.Id}, Name : {content.Name}, Contact : {content.Contact}, Time : {DateTime.Now.ToString("dd-MM-yy HH:mm:ss")}");
+                }
+            }
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
         }
     }
 }
