3b42552cb42932bd5421e43de542342e5d39b329
garryw28 <garryw2807@gmail.com>
2020-11-27 12:10:53 +0700

    initial-commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GrpcClientStreaming
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
